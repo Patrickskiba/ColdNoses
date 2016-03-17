85b9067b5b33794ff567b04093a9eb9cbf8a46ba
miguelito20 <lauerboy1@gmail.com>
2016-03-17 20:50:31 +0000

    Fixing production email variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.stmp_settings = {
+    address: "stmp.gmail.com", #gmail settings, needed to change if using a different mail service
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
   resources :contact_us, only: [:new, :create]
+  resources :maillist, only: [:new, :create]
   resources :events do
     collection do
       get 'list'
