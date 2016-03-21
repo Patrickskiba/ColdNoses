f357ce0ed0686ff081de00436007297a3c8f6a2d
Michael <lauerboy1@gmail.com>
2016-03-20 20:36:05 -0400

    removing carrier wave attributes in model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,12 +1,4 @@
 class Dog < ApplicationRecord
-  belongs_to :user
-  mount_uploader :image, ImageUploader
-  validate :picture_size
 
-  def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB")
-    end
-  end
 
 end
