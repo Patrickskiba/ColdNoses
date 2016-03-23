c1a9d4401e946f7c2f8d170804e1315be99bb013
Michael <lauerboy1@gmail.com>
2016-03-23 19:50:45 -0400

    editing events to allow for additions without having to be logged in for data entry purposes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  #before_action :set_event, only: [:show, :edit, :update, :destroy]
   #before_filter :authenticate_user!, :except => [:index, :home, :show, :list]
   # GET /events
   # GET /events.json
