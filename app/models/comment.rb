4b778e6d0147155720167b2f2dfe4aba7df21665
Michael <lauerboy1@gmail.com>
2016-03-29 12:49:27 -0400

    creating simple layout for comment lyfe

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  validates :title, presence: true, length: {minimum: 5, maximum: 35}
+  validates :text, presence: true, length: {minimum: 15, maximum: 500}
 end
