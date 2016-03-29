8541ec9d6af6cd84d2d22bb668f057666acbd69e
Michael <lauerboy1@gmail.com>
2016-03-29 17:22:08 -0400

    removing syntax for controller linking to user account

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,13 @@ class CommentsController < ApplicationController
     def create
       @event = Event.find(params[:event_id])
       @user = User.find(params[:user_id])
-      @comment = @event.comments.create(comment_params).includes(:user)
+      @comment = @event.comments.create(comment_params)
       redirect_to event_path(@event)
     end
     def edit
       @event = Event.find(params[:event_id])
       @user = User.find(params[:user_id])
-      @comment = @event.comments.find(params[:id]).includes(:user)
+      @comment = @event.comments.find(params[:id])
 
     end
     def destroy
