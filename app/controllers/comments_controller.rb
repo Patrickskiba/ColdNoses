e80797fd425b52674b518dc59ae5b3e1ac9be5c6
Michael <lauerboy1@gmail.com>
2016-03-29 18:14:32 -0400

    commenting out user linkage

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,19 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, :except => [:destroy, :create, :edit]
     def create
       @event = Event.find(params[:event_id])
-      @user = User.find(params[:user_id])
+   #   @user = User.find(params[:id])
       @comment = @event.comments.create(comment_params)
       redirect_to event_path(@event)
     end
     def edit
-      @event = Event.find(params[:event_id])
-      @user = User.find(params[:user_id])
+     # @event = Event.find(params[:event_id])
+      @user = User.find(params[:id])
       @comment = @event.comments.find(params[:id])
 
     end
     def destroy
       @event = Event.find(params[:event_id])
-      @user = User.find(params[:user_id])
+  #    @user = User.find(params[:id])
       @comment = @event.comments.find(params[:id])
       @comment.destroy
       redirect_to event_path(@event)
