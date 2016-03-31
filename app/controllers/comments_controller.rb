cdbd0b5ceddb210fb39c0f90192aabbca43e39d6
Michael <lauerboy1@gmail.com>
2016-03-31 11:38:29 -0400

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,22 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!, :except => [:destroy, :create, :edit]
     def create
       @event = Event.find(params[:event_id])
-   #   @user = User.find(params[:id])
       @comment = @event.comments.create(comment_params)
+      @comment.save
       redirect_to event_path(@event)
     end
     def edit
-     # @event = Event.find(params[:event_id])
-      @user = User.find(params[:id])
-      @comment = @event.comments.find(params[:id])
+      @comment = @event.comments.find(params[:event_id])
 
     end
     def destroy
       @event = Event.find(params[:event_id])
-  #    @user = User.find(params[:id])
-      @comment = @event.comments.find(params[:id])
+      @comment = @event.comments.find(comment_params)
       @comment.destroy
       redirect_to event_path(@event)
     end
       private
       def comment_params
-        params.require(:comment).permit(:user_name,:title, :comment)
+        params.require(:comment).permit(:title, :comment)
       end
   end
