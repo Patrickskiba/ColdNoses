064616a84f7163583a64ce5da634156435e1d42e
Michael <lauerboy1@gmail.com>
2016-04-09 00:56:58 -0400

    testing db connection

--- app/models/mail_list.rb
+++ app/models/mail_list.rb
@@ -7,6 +7,5 @@ class MailList < ApplicationRecord
     list_id = Rails.application.secrets.mailchimp_list_id
     result = mailchimp.list(list_id).members.create(body:{email_address: :user.email, status: 'subscribed', merge_fields: {FNAME: :user.first_name, LNAME: :user.last_name}})
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-    puts result
   end
 end
